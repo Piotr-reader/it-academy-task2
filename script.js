25842e6a6c5286ccda7e23b3c883ab8120abb6af
Piotr-reader <76643995+Piotr-reader@users.noreply.github.com>
2022-11-22 22:19:53 +0300

    implement regex function A and F

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let n = 1000;
+const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let a = "A";
+let b = "F";
+let sortArray = [];
+let itter = 0;
 
-nextPrime:
-for (let i = 2; i <= n; i++) {
-    for (let j = 2; j < i; j++) {
-        if (i % j == 0) continue nextPrime;
+const REG_FUNC = (arg1, arg2) => {
+  const REG_EX = new RegExp(`[${arg1}-${arg2}]`);
+  for (let i = 0; i < ALPHABET.length; i++) {
+    if (ALPHABET[i].match(REG_EX)) {
+      sortArray.push(ALPHABET[i]);
     }
-    console.log(i);
-}
+  }
+};
+REG_FUNC(a, b);
+const intervalId = setInterval(function () {
+  if (itter < sortArray.length) {
+    console.log(sortArray[itter]);
+    itter++;
+  }
+}, 1000);
